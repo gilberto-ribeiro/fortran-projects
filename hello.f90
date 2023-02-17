c1960b2535d948b5554586443ffb91dd72d72e36
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-02-17 16:25:04 -0300

    Icluding the age info

--- hello.f90
+++ hello.f90
@@ -3,13 +3,15 @@ program hello
     implicit none
     
     character(len=100) :: name
+    integer :: age
     
     ! Pergunta pelo nome.
-    print *, 'What is your name?'
-    read *, name
+    print *, 'What is your name? How old are you?'
+    read *, name, age
     
     ! Imprime mensagens no terminal.
     print *, 'Hello World, my name is ', trim(name), '.'
     print *, 'Hello ', trim(name), ', my name is World.'
+    print *, 'You were born in', 2023 - age, '.'
     
 end program hello
