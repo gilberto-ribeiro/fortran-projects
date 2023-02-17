70cf7802eca790cd2d4da28925fe653ced44f357
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-02-17 16:02:53 -0300

    Adding comments

--- hello.f90
+++ hello.f90
@@ -4,9 +4,11 @@ program hello
     
     character(len=100) :: name
     
+    ! Pergunta pelo nome.
     print *, 'What is your name?'
     read *, name
     
+    ! Imprime mensagens no terminal.
     print *, 'Hello World, my name is ', trim(name), '.'
     print *, 'Hello ', trim(name), ', my name is World.'
     
