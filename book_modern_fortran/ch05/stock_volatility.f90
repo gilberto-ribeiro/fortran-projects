3a351ed7673116973febf53bfa29b72013031428
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-02-24 23:06:15 -0300

    Adding empty code stock_volatility.f90

---
+++ book_modern_fortran/ch05/stock_volatility.f90
@@ -0,0 +1,13 @@
+program stock_volatility
+
+    use iso_fortran_env, only: int32, real32
+    use mod_arrays, only: moving_average, moving_std, &
+        average, std, reverse
+
+    implicit none
+
+
+
+contains
+
+end program stock_volatility
